ca65a556de1ef8c8ca708b1123bbf6cb38381ec3
marial-garces <m.garces2103@gmail.com>
2025-07-16 22:38:49 -0400

    letsgooo

--- app/src/main/java/com/example/countriesapp/di/NetworkModule.kt
+++ app/src/main/java/com/example/countriesapp/di/NetworkModule.kt
@@ -1,10 +1,15 @@
 package com.example.countriesapp.di
 
+import com.example.countriesapp.data.countries.remote.CountriesApi
+import com.example.countriesapp.data.countries.repository.CountriesRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
+import kotlin.jvm.java
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -12,4 +17,22 @@ object NetworkModule {
 
     @Provides
     @Singleton
+    fun provideRetroFit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://countriesnow.space/api/v0.1/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideCountriesApi(retrofit: Retrofit): CountriesApi {
+        return retrofit.create(CountriesApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCountriesRepository(api: CountriesApi): CountriesRepository {
+        return CountriesRepository(api)
+    }
 }
